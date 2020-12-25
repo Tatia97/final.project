65f2d04ebbf46f02a5479353d48aa10b2a05e18c
Tatia97 <tsiskarauli84@gmail.com>
2020-12-25 22:40:53 +0400

    final.project

--- signupscript.js
+++ signupscript.js
@@ -13,3 +13,18 @@ function myFunction3() {
     document.getElementById("hidden1").style.display='none';
     document.getElementById("hidden2").style.display='none';
 }
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (nameInput.value === '' || nameInput.value == null) {
+    messages.push('Name is required')
+  }
+  
+  if (emailInput.value === '' || emailInput.value == null) {
+    messages.push('Email is required')
+  }
+})
