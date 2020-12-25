3f84af828eb8cd6c1f12a8fe67f0f623dbdb4487
Tatia97 <tsiskarauli84@gmail.com>
2020-12-25 15:42:00 +0400

    final.project

---
+++ signupscript.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+    document.getElementById("hidden1").style.display='block';
+    document.getElementById("hidden2").style.display='none';
+    document.getElementById("hidden3").style.display='none';
+}
+function myFunction2() {
+    document.getElementById("hidden2").style.display='block';
+    document.getElementById("hidden1").style.display='none';
+    document.getElementById("hidden3").style.display='none';
+}
+function myFunction3() {
+    document.getElementById("hidden3").style.display='block';
+    document.getElementById("hidden1").style.display='none';
+    document.getElementById("hidden2").style.display='none';
+}
